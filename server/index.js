0236a75ec9f3a8a4a85cfaeae29d316f1d6a6ab1
Kenneth-Ye <102938628+Kenneth-Ye@users.noreply.github.com>
2024-01-15 15:39:24 -0500

    add text-to-speech for bot responses

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import OpenAI from 'openai';
 import * as dotenv from 'dotenv'
-
+import path from 'path'
+import fs from 'fs'
 
 const app = express();
 app.use(cors());
@@ -30,21 +31,31 @@ app.post('/', async(req, res) => {
             }
         );
         console.log("Successfully got response from openai")
-        
-        const ttsaudio = await openai.audio.speech.create({
-            model: "tts-1",
-            voice: "alloy",
-            input: response.choices[0].message.content,
-          });
-        
-        res.status(200).send({
+        res.status(200).json({
             gptresponse: response.choices[0].message.content,
-            audio: ttsaudio
         })
     } 
-    catch (error) {
+    catch(error) {
         console.log(error)
     }
 });
+
+app.post('/text-to-speech', async(req, res) => {
+    try {
+        const ttsaudio = await openai.audio.speech.create({
+            model: "tts-1",
+            voice: "alloy",
+            input: req.body.message,
+        });
+        const mp3 = Buffer.from(await ttsaudio.arrayBuffer());
+
+        const speechFile = path.resolve("./speech.mp3");
+        // const buffer = Buffer.from(await data.audio.arrayBuffer());
+        await fs.promises.writeFile(speechFile, mp3);
+        res.status(200).sendFile("src/audioFile.mpe", "./speech.mp3");
+    } catch(error) {
+        console.log(error)
+    }
+})
 
 app.listen(3000, () => console.log('Server is running on port 3000'))
